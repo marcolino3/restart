76ef73fb5ea6654d9348761df55f7e8b192de252
Marco Marranchelli <marco.marranchelli@me.com>
2025-08-18 16:41:27 +0200

    Entferne nicht verwendete Benutzerformular-Komponente und zugehörige Aktionsdatei

--- features/users/actions/update-user.action.ts
+++
@@ -1,8 +0,0 @@
-import { UserFormType } from "../schemas/user-form.schema";
-
-export const updateUserAction = async (
-  values: Partial<UserFormType> & { id: string }
-) => {
-  console.log(values);
-  return { success: true };
-};
